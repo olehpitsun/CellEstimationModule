483d9a06d44ae37812a9456ae3e8d412aab5d122
Oleh Pitsun <pichyn7@gmail.com>
2016-07-17 19:13:56 +0300

    add texture

--- src/sample/controller/StartController.java
+++ src/sample/controller/StartController.java
@@ -24,6 +24,7 @@ import sample.Main;
 import sample.model.DataBase;
 import sample.model.Filters.FilterColection;
 import sample.model.Nuclei;
+import sample.model.PreProcessing.PreProcessingOperation;
 import sample.model.ResearchParam;
 import sample.tools.ImageOperations;
 import sample.core.DB;
@@ -40,6 +41,8 @@ import java.util.List;
 import sample.util.PreProcessingParam;
 import javax.sql.DataSource;
 import java.sql.Statement;
+
+import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
 
 public class StartController {
@@ -129,7 +132,7 @@ public class StartController {
     @FXML
     public void folder(){
 
-        String f = "C:\\IMAGES\\mask_nuclei_(16042015_gisto_neproliferatyvna_mastopotia)";
+        String f = "C:\\IMAGES\\test";
 
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(f))) {
             for (Path file: stream) {
@@ -262,8 +265,13 @@ public class StartController {
                     * Imgproc.arcLength(contour2f, true);
 
 
-
             circular = Math.round(circular * 100.0) / 100.0;
+
+            System.out.println(" contourArea = " + contourArea);
+            System.out.println(" perimetr = " + perimetr);
+            System.out.println(" circular = " + circular);
+
+
             /**
              * блок підрахунку xc, yc, major_axis, minor_axis, theta
              * якщо площа більше 2. то все йде норм, інакше 0 , щоб не викидало помилок
@@ -271,13 +279,38 @@ public class StartController {
             mMOP2f1 = new MatOfPoint2f();
             contours.get(i).convertTo(mMOP2f1, CvType.CV_32FC2);
 
+            mMOP2f1.get(5,6);
             if(contourArea > 2) {
                 RotatedRect e = Imgproc.fitEllipse(mMOP2f1);
                 xc = e.center.x;
                 yc = e.center.y;
-                major_axis = e.size.height;    // width >= height
-                minor_axis = e.size.width;
+
+                if(e.size.height >= e.size.width){
+                    major_axis = e.size.height;    // width >= height
+                    minor_axis = e.size.width;
+                }else{
+                    major_axis = e.size.width ;
+                    minor_axis = e.size.height;
+                }
+
                 theta = e.angle;
+                e.boundingRect().area();
+
+                System.out.println(" xc = " + xc);
+                System.out.println(" yc = " + yc);
+                System.out.println(" major_axis = " + major_axis);
+                System.out.println(" minor_axis = " + minor_axis);
+                System.out.println(" angle = " + theta);
+                System.out.println(" Bx = " + e.boundingRect().x);
+                System.out.println(" By = " + e.boundingRect().y);
+                System.out.println(" B width = " + e.boundingRect().width);
+                System.out.println(" B height = " + e.boundingRect().height);
+                System.out.println(" Площа прямокутника = " + e.boundingRect().area());
+                System.out.println(" Aspect ratio = " + major_axis/minor_axis);
+                System.out.println(" Roudness = " + 4*contourArea / (Math.PI * pow(major_axis, 2)) );
+                System.out.println(" compactness  = " + Math.pow(perimetr,2) / 4*Math.PI * contourArea );
+
+
             }else{
                 xc = 0;
                 yc = 0;
@@ -286,6 +319,7 @@ public class StartController {
                 theta = 0;
             }
             equiDiameter = sqrt(4 * contourArea / Math.PI);
+            System.out.println(" equiDiameter = " + equiDiameter);
 
             Connection con ;
             com.mysql.jdbc.PreparedStatement stmt = null;
@@ -326,6 +360,179 @@ public class StartController {
         }
         nucleiTable.setItems(getNucleiData());
         this.setOriginalImage(drawing);
+    }
+
+    @FXML
+    public void getMask(){
+        Mat image = Highgui.imread("C:\\IMAGES\\test\\JC30O.png");
+        Mat mask = Highgui.imread("C:\\IMAGES\\test\\mask1.png", Highgui.CV_LOAD_IMAGE_GRAYSCALE);
+
+        Rect rectangle = new Rect(10, 10, image.cols() - 20, image.rows() - 20);
+
+        Mat bgdModel = new Mat(); // extracted features for background
+        Mat fgdModel = new Mat(); // extracted features for foreground
+        Mat source = new Mat(1, 1, CvType.CV_8U, new Scalar(0));
+
+        convertToOpencvValues(mask); // from human readable values to OpenCV values
+
+        int iterCount = 1;
+        Imgproc.grabCut(image, mask, rectangle, bgdModel, fgdModel, iterCount, Imgproc.GC_INIT_WITH_MASK);
+
+
+        convertToHumanValues(mask); // back to human readable values
+        Imgproc.threshold(mask,mask,0,128,Imgproc.THRESH_TOZERO);
+
+        Mat foreground = new Mat(image.size(), CvType.CV_8UC1, new Scalar(255, 255, 255));
+        image.copyTo(foreground, mask);
+
+this.setOriginalImage(foreground);
+
+//------------------------------------------------------------------------------------------------------------------
+
+        Mat src_gray = new Mat();
+        Imgproc.cvtColor(foreground, src_gray, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.blur(src_gray, src_gray, new Size(3, 3));
+
+        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+        Mat hierarchy = new Mat();
+        Mat mMaskMat = new Mat();
+
+        Scalar lowerThreshold = new Scalar ( 0, 0, 0 );
+        Scalar upperThreshold = new Scalar ( 10, 10, 10 );
+        Core.inRange(foreground, lowerThreshold, upperThreshold, mMaskMat);
+
+        Imgproc.findContours(mMaskMat, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+        List<Moments> mu = new ArrayList<Moments>(contours.size());
+        List<Point> mc = new ArrayList<Point>(contours.size());
+        Mat drawing = Mat.zeros( mMaskMat.size(), CvType.CV_8UC3 );
+
+        Rect rect ;
+
+        for( int i = 0; i< contours.size(); i++ ) {
+            rect=null;
+            rect = Imgproc.boundingRect(contours.get(i));
+            //Rect ROI = Rect(rect.x ,rect.y, rect.width, rect.height);
+
+            Mat crop = foreground.submat(rect );
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            Mat rgba =crop; Mat tempMat = crop;
+            rgba = new Mat(crop.cols(), crop.rows(), CvType.CV_8UC3);
+            crop.copyTo(rgba);
+
+            List<Mat> hsv_planes_temp = new ArrayList<Mat>(3);
+            Core.split(tempMat, hsv_planes_temp);
+
+            double threshValue1 = PreProcessingOperation.getHistAverage(crop, hsv_planes_temp.get(0));
+            System.out.println("thresh " + i + " " +  threshValue1);
+
+            /*
+            Core.rectangle( foreground, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height), new Scalar(0,0,250), 3);
+            Highgui.imwrite("C:\\IMAGES\\test\\result.jpg", foreground);*/
+
+            Highgui.imwrite("C:\\IMAGES\\test\\img"+i+".jpg", crop);
+            mu.add(i, Imgproc.moments(contours.get(i), false));
+            mc.add(i, new Point(mu.get(i).get_m10() / mu.get(i).get_m00(), mu.get(i).get_m01() / mu.get(i).get_m00()));
+            MatOfPoint2f contour2f = new MatOfPoint2f(contours.get(i).toArray());
+            /** малювання обєктів**/
+            Imgproc.drawContours(drawing, contours, i, new Scalar(255, 0, 0), 4, 1, hierarchy, 0, new Point());
+            Core.circle(drawing, mc.get(i), 4, new Scalar(0, 0, 255), -1, 2, 0);
+            //////////////////////////////////////////////////////////////////////////////////////////////////////
+            Core.putText(drawing, Integer.toString(i), new Point(rect.x - 20, rect.y),
+                    Core.FONT_HERSHEY_TRIPLEX, 1.7, new Scalar(255, 255, 255));
+            /*** Занесення даних до бази даних*/
+            double contourArea, perimetr, i_height, i_width, circular, equiDiameter;
+            MatOfPoint2f mMOP2f1;
+
+            contourArea = Imgproc.contourArea(contours.get(i));
+            perimetr = Imgproc.arcLength(contour2f, true);
+            System.out.println("Square " + contourArea);
+            System.out.println("Perimetr " + perimetr);
+
+
+        }
+
+
+
+
+
+
+
+        //this.setOriginalImage(drawing);
+    }
+
+    public double getHistAverage(Mat hsvImg, Mat hueValues)
+    {
+        // init
+        double average = 0.0;
+        Mat hist_hue = new Mat();
+        // 0-180: range of Hue values
+        MatOfInt histSize = new MatOfInt(180);
+        List<Mat> hue = new ArrayList<>();
+        hue.add(hueValues);
+
+        // compute the histogram
+        Imgproc.calcHist(hue, new MatOfInt(0), new Mat(), hist_hue, histSize, new MatOfFloat(0, 179));
+
+        // get the average Hue value of the image
+        // (sum(bin(h)*h))/(image-height*image-width)
+        // -----------------
+        // equivalent to get the hue of each pixel in the image, add them, and
+        // divide for the image size (height and width)
+        for (int h = 0; h < 180; h++)
+        {
+            // for each bin, get its value and multiply it for the corresponding
+            // hue
+            average += (hist_hue.get(h, 0)[0] * h);
+        }
+
+        // return the average hue of the image
+        return average = average / hsvImg.size().height / hsvImg.size().width;
+    }
+
+    private static void convertToHumanValues(Mat mask) {
+        byte[] buffer = new byte[3];
+        for (int x = 0; x < mask.rows(); x++) {
+            for (int y = 0; y < mask.cols(); y++) {
+                mask.get(x, y, buffer);
+                int value = buffer[0];
+                if (value == Imgproc.GC_BGD) {
+                    buffer[0] = (byte) 255 ; // for sure background
+                } else if (value == Imgproc.GC_PR_BGD) {
+                    buffer[0] = (byte) 170 ; // probably background
+                } else if (value == Imgproc.GC_PR_FGD) {
+                    buffer[0] = 85; // probably foreground
+                } else {
+                    buffer[0] = 0; // for sure foreground
+
+                }
+                mask.put(x, y, buffer);
+            }
+        }
+    }
+
+    private static void convertToOpencvValues(Mat mask) {
+        byte[] buffer = new byte[3];
+        for (int x = 0; x < mask.rows(); x++) {
+            for (int y = 0; y < mask.cols(); y++) {
+                mask.get(x, y, buffer);
+                int value = buffer[0];
+                if (value >= 0 && value < 64) {
+                    buffer[0] = Imgproc.GC_BGD; // for sure background
+                } else if (value >= 64 && value < 128) {
+                    buffer[0] = Imgproc.GC_PR_BGD; // probably background
+                } else if (value >= 128 && value < 192) {
+                    buffer[0] = Imgproc.GC_PR_FGD; // probably foreground
+                } else {
+                    buffer[0] = Imgproc.GC_FGD; // for sure foreground
+
+                }
+                mask.put(x, y, buffer);
+            }
+        }
+
     }
 
     /**
